106e6eb3e30166558ab1f062d4809c28c5a2d54d
Shashi raj <shashiraj13457@gmail.com>
2023-06-17 06:38:28 +0530

    Assignment Question

--- A3/EligibleForVote.java
+++ A3/EligibleForVote.java
@@ -2,10 +2,23 @@ package A3;
 
 import java.util.Scanner;
 
-//Q3. A person is eligible to vote if his/her age is greater than ot equal to 18 .
+//Q3. A person is eligible to vote if his/her age is greater than or equal to 18 .
     // Define a method to find out if he/she is eligible to vote.
 public class EligibleForVote {
     public static void main(String[] args) {
-        Scanner
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the age: ");
+        int age = input.nextInt();
+        boolean vote = isEligible(age);
+        System.out.println(vote);
+
+    }
+
+    static boolean isEligible (int age){
+        if (age >= 18){
+            System.out.println("Eligible to vote");
+            return true;
+        }
+        return false;
     }
 }
