173210844258396ca76882256b9cd59afae28e56
Shashi raj <shashiraj13457@gmail.com>
2023-06-18 05:33:24 +0530

    Assignment questions

---
+++ A3/Factorial.java
@@ -0,0 +1,32 @@
+package A3;
+
+import java.util.Scanner;
+
+// Q. Write a program to print the factorial of a number by defining a method named 'Factorial'.
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int num = input.nextInt();
+        Factorial(num);
+    }
+
+    static void Factorial (int num){
+        if (num < 0){
+            System.out.println("Invalid number");
+//            return;
+        }
+        int value = 1;
+        int x = 1;
+        while(x >= num){
+            value = value * x;
+            x++;
+
+        }
+//        for (int i = 1; i <= num ; i++) {
+//            value = value * i;
+//        }
+        System.out.println(value);
+        //It is giving 1 even if you enter num 0 , Because you have already given the value = 1;
+    }
+}
